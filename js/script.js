2e3a7fadcbf3e46ae91e1596ae9232884a4a1e12
Tatyana <106532861+L1senochek@users.noreply.github.com>
2023-02-26 13:12:19 +0300

    update files project
    with settings custom and player

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ const slidePrev = document.querySelector('.slide-prev');
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
 const city = document.querySelector('.city');
 //
 const audio = new Audio();
@@ -28,14 +30,19 @@ const audioTrack = document.querySelector('.audio-track');
 const changeQuote = document.querySelector('.change-quote');
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
+const soundVolumeLine = document.querySelector('.sound-volume');
+const volumeTrack = document.querySelector('.volume-track');
 // const widthAudioTrack = document.querySelector('.audio-track').offsetWidth;
+const currentSongTitle = document.querySelector('.current-song-title');
 
 let randomNum;
 let isPlay = false;
 let playNum = 0;
 let updateTime;
 
-// time and date
+///////////////////////////////////////////////
+// 1. Time and Date
+
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
@@ -155,10 +162,13 @@ async function getWeather() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=fde70cac528e6b7a31ec5fe4f655c39e&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
+  console.log(data, data.main);
   weatherIcon.className = 'weather-icon owf';
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
   weatherDescription.textContent = data.weather[0].description;
+  wind.textContent = `Wind speed: ${data.main.humidity.toFixed(0)} m/s`;
+  humidity.textContent = `Humidity: ${data.main.humidity.toFixed(0)} %`;
 }
 
 function setCity(event) {
@@ -215,30 +225,37 @@ changeQuote.addEventListener('click', rotate);
 // 6. Audio player
 
 let previousSongPlay;
+let currentTimeSong = 0;
 
 function playAudio() {
   if (!isPlay) {
     clearInterval(updateTime);// audio.src = 'https://l1senochek.github.io/momentum/assets/sounds/Metal%20Hellsinger%20-%20No%20Tomorrow%20ft%20Serj%20Tankian%20from%20System%20of%20a%20Down.mp3';
     // audio.src = '../assets/sounds/Jin Hashimoto - Stand Proud.mp3';
-    resetTime();
+    console.log('updateTime', updateTime);
     audio.src = playList[playNum].src;
+    currentSongTitle.innerHTML = playList[playNum].title;
     previousSongPlay = playNum;
     addBtn();
+    audio.currentTime = currentTimeSong;
+    if (audio.currentTime === 0) {
+      progressTraking();
+    } 
     audio.play();
     removeItemActive();
     let selectedSong = document.querySelector(`.class${playNum}`);
     selectedSong.classList.add('item-active');
     isPlay = true;
-    console.log(playNum);
     updateTime = setInterval(updateTimeSong, 1000);
   } else if (previousSongPlay === playNum && isPlay) {
     previousSongPlay = playNum;
     isPlay = false;
     removeBtn();
     audio.pause();
+    currentTimeSong = audio.currentTime;
   } else {
     audio.src = playList[playNum].src;
     addBtn();
+    audio.currentTime = currentTimeSong;
     audio.play();
     isPlay = true;
     removeItemActive();
@@ -324,10 +341,13 @@ function removeItemActive() {
   let selectedSong = document.querySelectorAll('.item-active');
   selectedSong.forEach(el => {
     el.classList.remove('item-active');
+    /////////
   });
 }
 
 function clickSongPlay() {
+  currentTimeSong = 0;
+  progressTraking();
   playNum = ((this.className).toString()).slice(15, 16);
   playAudio();
 }
@@ -335,24 +355,21 @@ function clickSongPlay() {
 console.log('duration', playNum, playList[playNum].duration)
 //convert
 
-function resetTime() {
-  trackTime.textContent = "00:00";
-  durationTime.textContent = "00:00";
-  audioTrack.value = 0;
-}
 let audioTrackId = document.getElementById('audioTrackId');
 audioTrackId.addEventListener('change', trakingSong);
 
 function trakingSong() {
   let trakingSong = audio.duration * (audioTrack.value / 100);
   audio.currentTime = trakingSong;
-}
+} 
 
 function updateTimeSong() {
   let clickPositionTime = 0; 
   if (!isNaN(audio.duration)) {
     clickPositionTime = audio.currentTime * (100 / audio.duration);
     audioTrack.value = clickPositionTime;
+    
+    currentTimeSong = audio.currentTime;
 
     let currentMin = Math.floor(audio.currentTime / 60);
     let currentSec = Math.floor(audio.currentTime - currentMin * 60);
@@ -374,6 +391,57 @@ function updateTimeSong() {
 
     trackTime.textContent = currentMin + ":" + currentSec;
     durationTime.textContent = durationMin + ":" + durationSec;
-  
+    
+
   }
 }
+ 
+// sound volume
+let soundVolumeId = document.getElementById('soundVolumeId');
+soundVolumeId.addEventListener('change', soundVolume);
+
+function soundVolume() {
+  audio.volume = soundVolumeLine.value / 100;
+}
+
+volumeTrack.addEventListener('click', volumeTrackMute);
+
+function volumeTrackMute () {
+  if (audio.muted === true) {
+    volumeTrack.style.background = `url("../momentum/assets/img/volume_up.png")`;
+    audio.muted = false;
+  } else {
+    volumeTrack.style.background = `url("../momentum/assets/img/mute.png")`;
+    audio.muted = true;
+  }
+}
+
+//progress tracking
+
+for (let el of document.querySelectorAll('input[type="range"].sound-volume-progress')) {
+  el.style.setProperty('--value', el.value);
+  el.style.setProperty('--min', el.min == '' ? '0' : el.min);
+  el.style.setProperty('--max', el.max == '' ? '100' : el.max);
+  el.addEventListener('input', () => el.style.setProperty('--value', el.value));
+}
+function progressTraking() {
+  audioTrack.value = 0;
+  for (let element of document.querySelectorAll('input[type="range"].audio-progress')) {
+    element.style.setProperty('--value', element.value);
+    element.style.setProperty('--min', element.min == '' ? '0' : element.min);
+    element.style.setProperty('--max', element.max == '' ? '100' : element.max);
+    element.addEventListener('input', () => element.style.setProperty('--value', element.value));
+  }
+}
+progressTraking();
+
+// 8. translation (en/ru or en/be)
+
+let greetingTranslation = {
+  en: ['Good',],
+  ru: ['Добрый'],
+  be: ['Добры']
+}
+
+
+
